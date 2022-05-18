4d5f46098b5a39a8b4e1e5806d854387b03ac968
lacma2011 <lacma2011@github.com>
2022-05-17 17:52:42 -0700

    db seed

--- docker-entrypoint-rails.sh
+++ docker-entrypoint-rails.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup
+bundle exec rake db:migrate db:seed 2>/dev/null || bundle exec rake db:setup
 
 rm -f tmp/pids/server.pid && bundle exec rails server -b $HOST
